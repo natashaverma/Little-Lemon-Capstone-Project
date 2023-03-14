b372fa033dd9493b25854ec145e0328a086f826f
Natasha Verma <naverma@adobe.com>
2023-03-14 18:49:35 +0530

    Added assets and custom navbar and footer files

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
+import NavBar from "./Components/Nav";
+import Footer from "./Components/Footer";
+import { Router, Routes, Route } from 'react-router';
+// import Header from "./Components/Header";
+// import Main from "./Components/Main";
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    < div className="App" >
+      < NavBar />
+      <Footer />
+    </ div>
   );
 }
 
---
+++ little-lemon/src/Components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/Logo.svg';
+
+function NavBar() {
+    return (
+        <nav>
+            <img src={Logo} alt="Little Lemon Logo" />
+            <ul>
+                {/* <li>
+                    <Link to="/">Home</Link>
+                </li> */}
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#menu">Menu</a>
+                </li>
+                {/* <li>
+                    <Link to="/booking">Reservations</Link>
+                </li> */}
+                <li>
+                    <a href="#order-online">Order Online</a>
+                </li>
+                <li>
+                    <a href="#login">Login</a>
+                </li>
+            </ul>
+        </nav >
+    );
+}
+
+export default NavBar;
